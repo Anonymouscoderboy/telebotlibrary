34f6e8fc959e5500d8dd9b6b4bac4551268af18b
Octave_Support <104682259+Anonymouscoderboy@users.noreply.github.com>
2023-05-26 00:01:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,40 @@
-from telegram.ext import Updater
 import logging
+import telebot
 
 TOKEN = "6101196560:AAE9Te6XfIfldcJcqdnh6Yb7SHPVi_z3hRc"
-CHAT_DATA_FILE = "chat_data.txt"
 
 # Configure logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Create a new TeleBot instance
+bot = telebot.TeleBot(TOKEN)
 
-def store_chat_data(update, context):
-    chat_id = update.effective_chat.id
-    user_id = update.effective_user.id
-    message = update.message.text
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def store_chat_data(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    text = message.text
 
     # Check if the message is a reply to a previous message
-    if update.message.reply_to_message:
-        # Retrieve the ID of the message being replied to
-        replied_message_id = update.message.reply_to_message.message_id
-
-        # Retrieve the text of the replied message
-        replied_message = update.message.reply_to_message.text
+    if message.reply_to_message:
+        replied_message_id = message.reply_to_message.message_id
+        replied_message = message.reply_to_message.text
 
         # Store the question, reply, and user information in a file
-        with open(CHAT_DATA_FILE, "a") as file:
+        with open("chat_data.txt", "a") as file:
             file.write(f"Chat ID: {chat_id}\n")
             file.write(f"User ID: {user_id}\n")
             file.write(f"Question: {replied_message}\n")
-            file.write(f"Reply: {message}\n")
-            file.write("--------------------\n")
-
-        logger.info("Chat data stored successfully.")
+            file.write(f"Reply: {text}\n")
+            file.write("--------\n")
 
 
 def main():
-    updater = Updater(TOKEN)
-    dispatcher = updater.dispatcher
-
-    # Register the message handler to analyze and store chat data
-    dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), store_chat_data))
-
-    updater.start_polling()
-    logger.info("Bot started polling...")
-    updater.idle()
+    # Start the bot
+    bot.polling()
 
 
 if __name__ == '__main__':
