41e0dd0dee94e468d228c619a3b372d7ff490ea9
Octave_Support <104682259+Anonymouscoderboy@users.noreply.github.com>
2023-05-26 10:01:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,41 @@
 import logging
-import os
-import pymongo
-from telegram.ext import Updater, MessageHandler, Filters
+import telebot
 
-TOKEN = '6101196560:AAE9Te6XfIfldcJcqdnh6Yb7SHPVi_z3hRc'
-MONGO_URI = 'mongodb+srv://abc:abcd@cluster0.dzijde4.mongodb.net/?retryWrites=true&w=majority'  # Update with your MongoDB connection URI
-DATABASE_NAME = 'abc'
-COLLECTION_NAME = 'AIdata'
+TOKEN = "6101196560:AAE9Te6XfIfldcJcqdnh6Yb7SHPVi_z3hRc"
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+# Configure logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-mongo_client = pymongo.MongoClient(MONGO_URI)
-db = mongo_client[DATABASE_NAME]
-collection = db[COLLECTION_NAME]
+# Create a TeleBot instance
+bot = telebot.TeleBot(TOKEN)
 
 
-def store_chat_data(update, context):
-    chat_id = update.effective_chat.id
-    user_id = update.effective_user.id
-    message_text = update.message.text
+@bot.message_handler(func=lambda message: True)
+def store_chat_data(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    text = message.text
 
-    data = {
-        'chat_id': chat_id,
-        'user_id': user_id,
-        'message_text': message_text
-    }
-
-    collection.insert_one(data)
+    # Store the chat data in your desired format (e.g., save to a file, database, etc.)
+    # Custom code for storing the chat data goes here
 
 
-def get_stored_reply(message_text):
-    stored_reply = collection.find_one({'message_text': message_text})
-    if stored_reply:
-        return stored_reply['reply']
-    else:
-        return None
+@bot.message_handler(func=lambda message: True)
+def get_stored_reply(message):
+    chat_id = message.chat.id
+    text = message.text
 
+    # Retrieve the stored reply based on the incoming message
+    # Custom code for retrieving the stored reply goes here
 
-def reply_to_message(update, context):
-    message_text = update.message.text
-
-    stored_reply = get_stored_reply(message_text)
-    if stored_reply:
-        update.message.reply_text(stored_reply)
+    # Reply to the message with the stored reply
+    bot.reply_to(message, stored_reply)
 
 
 def main():
-    updater = Updater(TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
-
-    # Register the message handler to store chat data
-    dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), store_chat_data))
-
-    # Register the message handler to reply based on stored replies
-    dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), reply_to_message))
-
-    updater.start_polling()
-    logger.info('Bot started polling...')
-
-    updater.idle()
+    bot.polling()
 
 
 if __name__ == '__main__':
