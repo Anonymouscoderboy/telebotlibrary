b11b20d586991e30b8efa6997c25e15d6a4b883a
Octave_Support <104682259+Anonymouscoderboy@users.noreply.github.com>
2023-05-26 10:18:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,36 @@
 import logging
-import os
 import telebot
 
 TOKEN = '6101196560:AAE9Te6XfIfldcJcqdnh6Yb7SHPVi_z3hRc'
-LOG_GROUP_ID = '-1001832126466'  # Replace with the ID of your private log group
+CHAT_ID = '-1001832126466'
+LOG_FILE = 'chat_log.txt'
 
-# Configure logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# Create a TeleBot instance
 bot = telebot.TeleBot(TOKEN)
 
-# Handler for storing chat data
-@bot.message_handler(func=lambda message: True)
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Store chat data in a log file
 def store_chat_data(message):
-    # Retrieve chat data
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    text = message.text
+    with open(LOG_FILE, 'a', encoding='utf-8') as file:
+        file.write(f'[{message.chat.id}] [{message.from_user.username}]: {message.text}\n')
 
-    # Store the chat data in the log group
-    log_message = f"Chat ID: {chat_id}\nUser ID: {user_id}\nMessage: {text}"
-    bot.send_message(LOG_GROUP_ID, log_message)
+# Load stored replies from the log file
+def load_stored_replies():
+    with open(LOG_FILE, 'r', encoding='utf-8') as file:
+        return file.readlines()
 
-    # Check if the bot is mentioned in the message
-    if bot.get_me().username.lower() in text.lower():
-        reply = get_stored_reply(text)
-        if reply:
+# Handle incoming messages
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    store_chat_data(message)
+
+    stored_replies = load_stored_replies()
+    for reply in stored_replies:
+        if reply.strip().lower() == message.text.strip().lower():
             bot.reply_to(message, reply)
-        else:
-            bot.reply_to(message, "Sorry, I don't have any stored replies.")
-
-# Function for retrieving stored reply
-def get_stored_reply(message):
-    # Implement your logic to retrieve stored replies
-    # based on the incoming message
-
-    # Example implementation: Check if the message is a known question
-    if message.lower() == "how are you?":
-        return "I'm fine, thank you!"
-
-    # If no stored reply is found, return None
-    return None
+            break
 
 # Start the bot
 bot.polling()
