ed977dca7b2f5a4b83556b7754ebe55ec2ff5d0a
Octave_Support <104682259+Anonymouscoderboy@users.noreply.github.com>
2023-05-26 10:50:40 +0530

    Update reply.py

--- reply.py
+++ reply.py
@@ -1,7 +1,4 @@
-start_reply = "Welcome to the bot! This is the start message."
-help_reply = "Here is some help information."
-info_reply = "Here is some information about the bot."
-status_reply = "The bot is currently active and running."
-Tum kon = "Mai bot hu"
-Tum kha sai ho = "Mai BotWorld sai aya hu"
-# Add more replies for additional commands
+how_are_you_reply = "I'm doing well, thank you!"
+name_reply = "My name is Bot. Nice to meet you!"
+Tum_kon_horeply = "Mera name charlie hai or mai botland sai aya hu!"
+# Add more predefined replies for specific questions
