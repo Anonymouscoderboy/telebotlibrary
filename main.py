13de90ce589a6eec320343c7b298db06599f903a
Octave_Support <104682259+Anonymouscoderboy@users.noreply.github.com>
2023-05-26 21:18:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,19 @@ replies = {
     'tum kon ho': 'Main ek Telegram bot hoon.',
     'thumra name kya hai': 'Mera naam ChatBot hai.',
     'where are you from': 'I am from the virtual world.',
-    'tum kha rhete ho': 'Main har jagah maujood hoon!'
+    'tum kha rhete ho': 'Main har jagah maujood hoon!',
+    'kaise ho?': 'मैं ठीक हूँ, धन्यवाद।',
+    'tumhara naam kya hai?': 'मेरा नाम ChatBot है।',
+    'aap kaha se ho?': 'मैं एक AI आधारित बॉट हूँ और मेरी स्थानवाली सर्वर पर स्थापित हूँ।',
+    'aapko kya pasand hai?': 'मुझे भाषा समझना और उपयोगकर्ताओं की मदद करना पसंद है।',
+    'aapka favorite color kya hai?': 'मेरा पसंदीदा रंग हरा है।',
+    'kya aap mujhe kuch bata sakte ho?': 'हाँ, मैं आपको विभिन्न विषयों पर जानकारी और मदद प्रदान कर सकता हूँ।',
+    'aapka kaam kya hai?': 'मेरा काम उपयोगकर्ताओं की बातचीत में सहायता करना है और उनके सवालों का उत्तर देना है।',
+    'mujhe madad chahiye': 'आप मुझसे किसी विषय पर सवाल पूछ सकते हैं और मैं आपकी मदद करने की कोशिश करूँगा।',
+    'aapka din kaisa raha?': 'मेरा दिन अच्छा रहा, धन्यवाद।',
+    'aapko koi bimari hai?': 'नहीं, मैं एक AI बॉट हूँ, मुझे बीमारी नहीं होती।',
+    'aap kya kar sakte ho?': 'मैं आपको विभिन्न विषयों पर जानकारी देने, सवालों का उत्तर देने और मदद करने में सक्षम हूँ।',
+    'kya aap pyaar karte ho?': 'हाँ, मैं आपसे प्यार करता हूँ ❤️"
 }
 
 # Additional questions and replies
