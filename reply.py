de5177369da4146d34fc212fad50806c4b4debdf
Octave_Support <104682259+Anonymouscoderboy@users.noreply.github.com>
2023-05-26 10:38:29 +0530

    Create reply.py

---
+++ reply.py
@@ -0,0 +1,7 @@
+start_reply = "Welcome to the bot! This is the start message."
+help_reply = "Here is some help information."
+info_reply = "Here is some information about the bot."
+status_reply = "The bot is currently active and running."
+Tum kon = "Mai bot hu"
+Tum kha sai ho = "Mai BotWorld sai aya hu"
+# Add more replies for additional commands
